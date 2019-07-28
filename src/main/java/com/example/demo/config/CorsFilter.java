83866f33f70f2f2a23ca759d7aafbc7e30800753
yz-zfd <564408462@qq.com>
2019-07-28 17:13:17 +0800

    <Herder> <refactor>:(<驾驶员管理模块>)设置跨域配置
    <Body>

---
+++ src/main/java/com/example/demo/config/CorsFilter.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        filterChain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
